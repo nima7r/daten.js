05bd454fc86381caf378d8f5c2adda40f922c39d
nima <n7r.distributed.ai@gmail.com>
2018-10-30 00:01:15 +0330

    Fix indentation problems

--- src/curves/ellipticcurve.js
+++ src/curves/ellipticcurve.js
@@ -5,11 +5,11 @@ module.exports = (function() {
 
 	function assert(condition, message) {
     if (!condition) {
-        message = message || "Assertion failed";
-        if (typeof Error !== "undefined") {
-            throw new Error(message);
-        }
-        throw message; // Fallback
+			message = message || "Assertion failed";
+			if (typeof Error !== "undefined") {
+					throw new Error(message);
+			}
+			throw message; // Fallback
     }
 	}
 
